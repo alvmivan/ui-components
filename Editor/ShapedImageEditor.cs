86d30b893776742e0cd93c8b02c86f92d7e92781
alvmivan <alvmivan@gmail.com>
2021-02-08 21:03:22 -0300

    add editor

---
+++ Editor/ShapedImageEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace UITools
+{
+    [CustomEditor(typeof(ShapedImage), true)]
+    [CanEditMultipleObjects]
+    public class ShapedImageEditor : ImageEditor
+    {
+        private SerializedProperty cornerDivisions;
+        private SerializedProperty amount;
+        private SerializedProperty desiredCornerRadius;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            cornerDivisions = serializedObject.FindProperty("cornerDivisions");
+            amount = serializedObject.FindProperty("amount");
+            desiredCornerRadius = serializedObject.FindProperty("desiredCornerRadius");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            EditorGUILayout.Separator();
+            
+            var modified = false;
+            modified |= EditorGUILayout.PropertyField(cornerDivisions);
+            modified |= EditorGUILayout.PropertyField(amount);
+            modified |= EditorGUILayout.PropertyField(desiredCornerRadius);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
